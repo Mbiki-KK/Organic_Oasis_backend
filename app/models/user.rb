e70cc02b07e1baa71a197045267ecec9c146259f
paul kabuu <kongkabuu9@gmail.com>
2023-07-31 14:45:52 +0300

    added auth controleer

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,19 @@
 class User < ApplicationRecord
   after_initialize :set_default_role
   enum role:[:user,:seller,:admin]
+  has_secure_password
+mount_uploder :avatar, AvatarUploder
+validates :email, presence:true, uniqueness: true
+validates :email, format:{with:URI::MailTO::EMAIL_REGEXP}
+validates :username, presence:true, uniqueness: true
+validates :password, length:{minimum:6}, if: -> {new_record?||  !password.nil?}
+
+
   has_one :address
   has_many :orders
   has_many :reviews
   has_many :products, through: :orders
-has_secure_password
+
   def set_default_role
 self.role ||= :user
   end
