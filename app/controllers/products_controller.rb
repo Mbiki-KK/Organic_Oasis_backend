2dbf8d5b604049981422baf14fe632968f0c3bd1
paul kabuu <kongkabuu9@gmail.com>
2023-08-02 12:36:42 +0300

    deleted unessesary migrations

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,19 +1,19 @@
-# class ProductsController < ApplicationController
-#   def index
-#     @products = Product.all
+class ProductsController < ApplicationController
+  def index
+    @products = Product.all
 
-#     render json: @products
-#   end
+    render json: @products
+  end
 
-#   def show
-#     @product = Product.find_by(id: params[:id])
+  def show
+    @product = Product.find_by(id: params[:id])
 
-#     render json: @product
-#   end
+    render json: @product
+  end
 
-#   def create
-#   end
+  def create
+  end
 
-#   def destroy
-#   end
-# end
+  def destroy
+  end
+end
