dda870d0089f1b1086f5e690a403429328132425
Mbiki-KK <wawerumbiki@gmail.com>
2023-08-03 11:29:40 +0300

    feat: added uplaod functionality to products profile

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -29,4 +29,11 @@ class ProductsController < ApplicationController
         render json: {error: "Product not found"}
       end
   end
+
+  private
+
+  def product_params
+    params.permit(:name, :desc, :price, :availability, :image, :category_id, :seller_id)
+  end
+
 end
