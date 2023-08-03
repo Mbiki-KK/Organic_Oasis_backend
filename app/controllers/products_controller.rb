a3f5553ef92e1ff56fa4adf026c376b01748fd3c
Mbiki-KK <wawerumbiki@gmail.com>
2023-08-03 10:23:01 +0300

    feat: done with controllers

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -12,8 +12,21 @@ class ProductsController < ApplicationController
   end
 
   def create
+    product = Product.new(product_params)
+      if product.save
+        render json: product, status: :ok
+      else
+        render json: { errors: "Cannot add product" }, status: :unprocessable_entity
+      end
   end
 
   def destroy
+    product = Product.find_by(id: params[:id])
+      if product
+        product.destroy
+        head :no_content
+      else
+        render json: {error: "Product not found"}
+      end
   end
 end
