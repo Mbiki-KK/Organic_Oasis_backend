2231d49b1b2464dcc3186ed7fb9a4cb71f40d7a7
Cassie <cassierasugu@gmail.com>
2023-08-05 23:55:47 +0300

    routes

--- config/routes.rb
+++ config/routes.rb
@@ -9,11 +9,17 @@ Rails.application.routes.draw do
   resources :users
   resources :orders
   resources :categories
+
+  # Routes for each category
   resources :meat, :beverage, :bakery, :spices, :cereal, :dairy, :offers, :veggies_and_fruits, only: [:index, :show, :create, :update, :destroy]
+
   # Route for refreshing the JWT token
   post '/refresh', to: 'sessions#refresh'
 
   # Image routes
   post '/upload_image', to: 'images#upload_image'
   post '/seller/products/:product_id/upload_image', to: 'seller/products#upload_image'
+
+  # Root route - Choose any category to be the root
+  root 'meat#index'
 end
