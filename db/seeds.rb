e1734680fd5923212206ce6125e969bbdd2188eb
Cassie <cassierasugu@gmail.com>
2023-08-05 21:52:11 +0300

    faker

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,3 +1,108 @@
+# data for each category product
+# db/seeds.rb
+
+# Create fake data for Meat
+5.times do
+  Meat.create(
+    name: Faker::Food.dish,
+    price: Faker::Commerce.price(range: 1..100.0),
+    image_url: Faker::Placeholdit.image,
+    description: Faker::Food.description
+  )
+end
+
+# Create fake data for Beverage
+5.times do
+  Beverage.create(
+    name: Faker::Drink.unique.beverage,
+    price: Faker::Commerce.price(range: 1..100.0),
+    image_url: Faker::Placeholdit.image,
+    description: Faker::Food.description
+  )
+end
+
+# Create fake data for Bakery
+5.times do
+  Bakery.create(
+    name: Faker::Food.dish,
+    price: Faker::Commerce.price(range: 1..100.0),
+    image_url: Faker::Placeholdit.image,
+    description: Faker::Food.description
+  )
+end
+
+# Create fake data for Spices
+5.times do
+  Spices.create(
+    name: Faker::Food.spice,
+    price: Faker::Commerce.price(range: 1..100.0),
+    image_url: Faker::Placeholdit.image,
+    description: Faker::Food.description
+  )
+end
+
+# Create fake data for Cereal
+5.times do
+  Cereal.create(
+    name: Faker::Food.dish,
+    price: Faker::Commerce.price(range: 1..100.0),
+    image_url: Faker::Placeholdit.image,
+    description: Faker::Food.description
+  )
+end
+
+# Create fake data for Dairy
+5.times do
+  Dairy.create(
+    name: Faker::Food.dish,
+    price: Faker::Commerce.price(range: 1..100.0),
+    image_url: Faker::Placeholdit.image,
+    description: Faker::Food.description
+  )
+end
+
+# Create fake data for Offers
+5.times do
+  Offers.create(
+    name: Faker::Commerce.product_name,
+    price: Faker::Commerce.price(range: 1..100.0),
+    image_url: Faker::Placeholdit.image,
+    description: Faker::Commerce.promotion
+  )
+end
+
+# Create fake data for VeggiesAndFruits
+5.times do
+  VeggiesAndFruits.create(
+    name: Faker::Food.fruits,
+    price: Faker::Commerce.price(range: 1..100.0),
+    image_url: Faker::Placeholdit.image,
+    description: Faker::Food.description
+  )
+end
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 require 'faker'
 # Clear existing data
 OrderItem.delete_all
