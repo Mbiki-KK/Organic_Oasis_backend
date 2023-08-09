6762c0994bf6192d4fa7360e80586f975c9c5219
Cassie <cassierasugu@gmail.com>
2023-08-09 08:33:26 +0300

    changes

--- app/controllers/sellers_controller.rb
+++ app/controllers/sellers_controller.rb
@@ -8,15 +8,13 @@ class SellersController < ApplicationController
     @sellers = Seller.all
     render json: @users, status: :ok, except: [:password_digest, :created_at, :updated_at]
   end
-
+# GET /users/{username}
   def show
     @seller = Seller.find(params[:id])
-
-
-    ender json: @seller
+    render json: @user, status: :ok
   end
-end
 
+ # POST /users
 def create
   @seller = Seller.new(seller_params)
   if seller.save
@@ -25,8 +23,27 @@ def create
     render json: { status: 'Error', message: 'Failed to register seller', errors: @seller.errors }, status: :unprocessable_entity
   end
 end
+# PUT /sellers/{sellersname}
+def update
+  unless @seller.update(user_params)
+    render json: { errors: @seller.errors.full_messages },
+           status: :unprocessable_entity
+  end
+end
+# DELETE
+def destroy
+  @seller.destroy
+end
+
 
 private
+
+def find_user
+  @seller = Seller.find_by_username!(params[:_username])
+  rescue ActiveRecord::RecordNotFound
+    render json: { errors: 'Seller not found' }, status: :not_found
+end
+
 
 def seller_params
   params.require(:seller).permit(:name, :email, :password, :password_confirmation)
